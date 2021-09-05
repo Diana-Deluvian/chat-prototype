6a1fd3e5b02c0ef984b93df900dce56cce0eb003
Diana Deluvian <diana.deluvian@gmail.com>
2021-09-05 15:55:00 +0200

    chat component, cors issue fixed

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
 
 import './Chat.css';
 
-const Chat = () => {
+let socket;
+
+const Chat = (props) => {
+    const [name, setName] = useState(''); 
+    const [room, setRoom] = useState(''); 
+    const ENDPOINT = 'localhost:5000';
+    useEffect(() => {
+        const { room, name } = queryString.parse(props.location.search);
+
+        socket = io(ENDPOINT);
+
+        setName(name);
+        setRoom(room);
+
+        socket.emit('join', { name, room }, () => {
+
+        });
+
+        return () => {
+            socket.emit('disconnect');
+            socket.off();
+        }
+    }, [ENDPOINT, props.location.search]);
+
     return <h1>Yo from chat</h1>
 }
 
--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
+var cors = require('cors');
+
+
 
 const PORT = process.env.PORT || 5000;
 
@@ -8,16 +11,28 @@ const router = require('./router');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+const io = socketio(server,{
+    cors:{
+        origin:'*',
+    }
+});
+
+app.use(cors());
 
 io.on('connection', (socket) => {
     console.log("We have a new connection", socket);
 
+    socket.on('join', ({ name, room }) => {
+        console.log(name, room);
+    }
+    );
+
     socket.on('disconnect', () => {
         console.log("User has left");
     })
-})
+});
 
 app.use(router);
+
 
 server.listen(PORT, () => console.log(`Server has started on port: ${PORT}`));
